0c8842da10a359fd97fffb0cead68cd1f2c91f4d
Hatef Barin <amir.barin76@gmail.com>
2025-03-18 18:38:16 +0330

    Add UserRegistrationSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -16,4 +16,35 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'email', 'username', 'first_name', 'last_name', 'date_of_birth', 'is_active', 'is_staff', 'date_joined')
-        
+
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for registering a new user. It handles password hashing.
+    """
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password', 'first_name', 'last_name', 'date_of_birth')
+
+
+    def validate_password(self, value):
+        """
+        Custom password validation to ensure password is strong enough.
+        """
+        if len(value) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        return value
+
+
+    def create(self, validated_data):
+        """
+        Create a new user with hashed password.
+        """
+        password = validated_data.pop('password')
+        user = CustomUser.objects.create(**validated_data)
+        user.set_password(password)  # Hash the password before saving
+        user.save()
+        return user
