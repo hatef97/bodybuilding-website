764694c9d88841d34a61402eda9e4937357cc0f5
Hatef Barin <amir.barin76@gmail.com>
2025-03-18 16:05:53 +0330

    Setup Core App

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'rest_framework',
+    # My apps
+    'core',
 ]
 
 MIDDLEWARE = [
