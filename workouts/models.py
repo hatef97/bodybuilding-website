bdb70db528c59f558fdcfb152938d21c628940d8
Hatef Barin <amir.barin76@gmail.com>
2025-03-22 20:57:38 +0430

    Add WorkoutPlan to models

--- workouts/models.py
+++ workouts/models.py
@@ -12,4 +12,19 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class WorkoutPlan(models.Model):
+    """
+    A model for a workout plan, containing multiple exercises.
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    exercises = models.ManyToManyField(Exercise, related_name='workout_plans')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
         
