350587badb725508e0a1b73ec194540375aa7d8d
Hatef Barin <amir.barin76@gmail.com>
2025-03-22 21:37:21 +0430

    Add Pagination settings

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Exercise, WorkoutPlan, WorkoutLog
+from .serializers import ExerciseSerializer, WorkoutPlanSerializer, WorkoutLogSerializer, UserWorkoutLogSerializer
+
+
+
+# Pagination settings
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10  # Number of items per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
