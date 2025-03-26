850fecdad3f1c8c42519aafc468267f336093187
Hatef Barin <amir.barin76@gmail.com>
2025-03-26 14:56:44 +0430

    Add CalorieCalculatorViewSet

--- nutrition/views.py
+++ nutrition/views.py
@@ -145,3 +145,50 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'destroy']:
             return [IsAdminUser()]  # Only admins can create, update, or destroy meal plans
         return [IsAuthenticated()]  # Any authenticated user can list or view meal plans
+
+
+
+class CalorieCalculatorViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for calculating daily calorie requirements based on user input.
+    """
+    queryset = CalorieCalculator.objects.all()
+    serializer_class = CalorieCalculatorSerializer
+    pagination_class = StandardResultsSetPagination  # Use pagination for large result sets
+
+    def get_permissions(self):
+        """
+        Allow authenticated users to perform CRUD operations on meal plans.
+        """
+        if self.action in ['create', 'update', 'destroy']:
+            return [IsAdminUser()]  # Only admins can create, update, or destroy meal plans
+        return [IsAuthenticated()]  # Any authenticated user can list or view meal plans
+
+
+    @action(detail=False, methods=['get'])
+    def calculate(self, request):
+        """
+        Custom action to calculate daily calories based on user input.
+        """
+        age = request.query_params.get('age')
+        weight = request.query_params.get('weight')
+        height = request.query_params.get('height')
+        activity_level = request.query_params.get('activity_level')
+        goal = request.query_params.get('goal')
+        gender = request.query_params.get('gender')
+
+        if not all([age, weight, height, activity_level, goal, gender]):
+            return Response({"detail": "All parameters must be provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+        calorie_calculator = CalorieCalculator(
+            age=int(age),
+            weight=float(weight),
+            height=float(height),
+            activity_level=activity_level,
+            goal=goal,
+            gender=gender
+        )
+
+        calories = calorie_calculator.calculate_calories()
+
+        return Response({"daily_calories": calories}, status=status.HTTP_200_OK)
