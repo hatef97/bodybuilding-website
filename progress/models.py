df82406d6643e1f6035bca8a443edef51068e870
Hatef Barin <amir.barin76@gmail.com>
2025-04-02 14:25:30 +0430

    Add WeightLog

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class WeightLog(models.Model):
+    """
+    Tracks a user's weight over time.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='weight_logs')
+    weight_kg = models.DecimalField(max_digits=5, decimal_places=2, help_text="Weight in kilograms")
+    date_logged = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_logged']
+        verbose_name = 'Weight Log'
+        verbose_name_plural = 'Weight Logs'
+        unique_together = ('user', 'date_logged')
+
+    def __str__(self):
+        return f"{self.user} - {self.weight_kg}kg on {self.date_logged}"
