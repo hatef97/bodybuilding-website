a80abf4108808fd3c52070a7ba564d1701c5fb7d
Hatef Barin <amir.barin76@gmail.com>
2025-04-02 14:27:00 +0430

    Add BodyMeasurement

--- progress/models.py
+++ progress/models.py
@@ -19,3 +19,28 @@ class WeightLog(models.Model):
 
     def __str__(self):
         return f"{self.user} - {self.weight_kg}kg on {self.date_logged}"
+
+
+
+class BodyMeasurement(models.Model):
+    """
+    Stores detailed body measurements for a user.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='body_measurements')
+    chest_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    waist_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    hips_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    biceps_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    thighs_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    calves_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    neck_cm = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    date_logged = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_logged']
+        verbose_name = 'Body Measurement'
+        verbose_name_plural = 'Body Measurements'
+        unique_together = ('user', 'date_logged')
+
+    def __str__(self):
+        return f"{self.user} measurements on {self.date_logged}"
