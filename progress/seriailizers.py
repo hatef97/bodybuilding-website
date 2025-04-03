425ce7f75fe14273a0bc8caf053c0ce78dece5a3
Hatef Barin <amir.barin76@gmail.com>
2025-04-03 14:40:32 +0430

    Add BodyMeasurementSerializer

--- progress/seriailizers.py
+++ progress/seriailizers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from django.utils import timezone
 
-from progress.models import WeightLog
+from .models import WeightLog, BodyMeasurement
 
 
 
@@ -26,3 +26,38 @@ class WeightLogSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("You have already logged your weight today.")
 
         return data
+
+
+
+class BodyMeasurementSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    date_logged = serializers.DateField(read_only=True)
+
+    class Meta:
+        model = BodyMeasurement
+        fields = [
+            'id',
+            'user',
+            'chest_cm',
+            'waist_cm',
+            'hips_cm',
+            'biceps_cm',
+            'thighs_cm',
+            'calves_cm',
+            'neck_cm',
+            'date_logged'
+        ]
+        read_only_fields = ['id', 'date_logged']
+
+    def validate(self, data):
+        """
+        Prevent duplicate measurements for the same user on the same date.
+        """
+        user = self.context['request'].user
+        today = timezone.now().date()
+
+        if BodyMeasurement.objects.filter(user=user, date_logged=today).exists():
+            raise serializers.ValidationError("You have already submitted body measurements for today.")
+
+        return data
+        
