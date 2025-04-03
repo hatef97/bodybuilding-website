e76c0ad1dadf986dc82a88e06113c599f8cf2d72
Hatef Barin <amir.barin76@gmail.com>
2025-04-03 14:42:59 +0430

    Add ProgressLogSerializer

--- progress/seriailizers.py
+++ progress/seriailizers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from django.utils import timezone
 
-from .models import WeightLog, BodyMeasurement
+from .models import WeightLog, BodyMeasurement, ProgressLog
 
 
 
@@ -60,4 +60,27 @@ class BodyMeasurementSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("You have already submitted body measurements for today.")
 
         return data
-        
+
+
+
+class ProgressLogSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    image = serializers.ImageField(required=False, allow_null=True)
+    date_logged = serializers.DateField(read_only=True)
+
+    class Meta:
+        model = ProgressLog
+        fields = ['id', 'user', 'title', 'note', 'image', 'date_logged']
+        read_only_fields = ['id', 'date_logged']
+
+    def validate(self, data):
+        """
+        prevent duplicate logs for the same day.
+        """
+        user = self.context['request'].user
+        today = timezone.now().date()
+
+        if ProgressLog.objects.filter(user=user, date_logged=today).exists():
+            raise serializers.ValidationError("You already have a progress log for today.")
+
+        return data
