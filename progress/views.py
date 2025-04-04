18a2371a650c5c23a43ac53e31a1121e90abc9a7
Hatef Barin <amir.barin76@gmail.com>
2025-04-04 19:08:58 +0430

    Add ProgressLogViewSet

--- progress/views.py
+++ progress/views.py
@@ -80,3 +80,36 @@ class BodyMeasurementViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(today_measurement)
             return Response(serializer.data)
         return Response({"detail": "No measurements logged for today."}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+class ProgressLogViewSet(viewsets.ModelViewSet):
+    serializer_class = ProgressLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        """
+        Returns the logs of the authenticated user, ordered by the most recent.
+        """
+        user = self.request.user
+        return ProgressLog.objects.filter(user=user).order_by('-date_logged')
+
+    def perform_create(self, serializer):
+        """
+        Automatically associate the logged-in user with the progress log entry.
+        """
+        serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def today(self, request):
+        """
+        Custom action to retrieve today's progress log (if any).
+        """
+        user = request.user
+        today_log = ProgressLog.objects.filter(user=user, date_logged=timezone.now().date()).first()
+        
+        if today_log:
+            serializer = self.get_serializer(today_log)
+            return Response(serializer.data)
+        return Response({"detail": "No log for today."}, status=status.HTTP_404_NOT_FOUND)
