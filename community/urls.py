590a83b54eb17dfcbb36b2b37951e30e5ae1cb78
Hatef Barin <amir.barin76@gmail.com>
2025-05-19 14:07:59 +0430

    Set up URLs for CommentViewSet

--- community/urls.py
+++ community/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ForumPostViewSet
+from .views import ForumPostViewSet, CommentViewSet
 
 
 
 router = DefaultRouter()
 router.register(r'forum_posts', ForumPostViewSet, basename='forumpost')
+router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = router.urls
