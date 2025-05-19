3758512469b448f2faef488c84b5375168f7c386
Hatef Barin <amir.barin76@gmail.com>
2025-05-19 16:27:07 +0430

    Set up URLs for ChallengeViewSet

--- community/urls.py
+++ community/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ForumPostViewSet, CommentViewSet
+from .views import ForumPostViewSet, CommentViewSet, ChallengeViewSet
 
 
 
 router = DefaultRouter()
 router.register(r'forum_posts', ForumPostViewSet, basename='forumpost')
 router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'challenges', ChallengeViewSet, basename='challenge')
 
 urlpatterns = router.urls
