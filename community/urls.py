8a7d1ae0649a148e73f0c29b264ec09b0e1249aa
Hatef Barin <amir.barin76@gmail.com>
2025-05-25 20:23:47 +0430

    Set up URLs for UserProfileViewSet

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ForumPostViewSet, CommentViewSet, ChallengeViewSet, LeaderboardViewSet
+from .views import ForumPostViewSet, CommentViewSet, ChallengeViewSet, LeaderboardViewSet, UserProfileViewSet
 
 
 
@@ -9,5 +9,6 @@ router.register(r'forum_posts', ForumPostViewSet, basename='forumpost')
 router.register(r'comments', CommentViewSet, basename='comment')
 router.register(r'challenges', ChallengeViewSet, basename='challenge')
 router.register(r'leaderboards', LeaderboardViewSet, basename='leaderboard')
+router.register(r'user_profiles', UserProfileViewSet, basename='userprofile')
 
 urlpatterns = router.urls
