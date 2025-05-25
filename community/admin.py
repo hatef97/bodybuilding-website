0ff0db670bd24bdc3be34bdb282b1e3dc0d0f78d
Hatef Barin <amir.barin76@gmail.com>
2025-05-25 20:40:54 +0430

    Add CommentAdmin

--- community/admin.py
+++ community/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 
-from community.models import ForumPost
+from community.models import ForumPost, Comment
 
 
 
@@ -80,3 +80,73 @@ class ForumPostAdmin(admin.ModelAdmin):
         updated = queryset.update(is_active=False)
         self.message_user(request, f"{updated} post(s) marked as inactive.")
     make_inactive.short_description = "Mark selected posts as Inactive"
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin panel configuration for Comment.
+    Allows quick moderation (activate/deactivate), filtering, and navigation.
+    """
+    list_display = (
+        "id",
+        "short_content",
+        "author_link",
+        "post_link",
+        "created_at",
+        "is_active",
+    )
+    list_display_links = ("id", "short_content")
+    list_editable = ("is_active",)
+    list_filter = ("is_active", "created_at", "user")
+    search_fields = ("content", "user__username", "post__title")
+    date_hierarchy = "created_at"
+    raw_id_fields = ("user", "post")
+    readonly_fields = ("created_at",)
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        (None, {
+            "fields": ("user", "post", "content", "is_active"),
+        }),
+        ("Meta", {
+            "classes": ("collapse",),
+            "fields": ("created_at",),
+        }),
+    )
+
+    actions = ["make_active", "make_inactive"]
+
+    def short_content(self, obj):
+        """Truncate long comments for display."""
+        text = obj.content
+        return (text[:75] + "...") if len(text) > 75 else text
+    short_content.short_description = "Comment"
+
+    def author_link(self, obj):
+        """Link to the comment’s author in the User admin."""
+        url = reverse("admin:core_customuser_change", args=[obj.user_id])
+        return format_html('<a href="{}">{}</a>', url, obj.user.username)
+    author_link.short_description = "Author"
+    author_link.admin_order_field = "user__username"
+
+    def post_link(self, obj):
+        """Link to the related forum post in the admin."""
+        url = reverse("admin:community_forumpost_change", args=[obj.post_id])
+        return format_html('<a href="{}">{}</a>', url, obj.post.title)
+    post_link.short_description = "Post"
+    post_link.admin_order_field = "post__title"
+
+    def make_active(self, request, queryset):
+        """Admin action to mark selected comments as active."""
+        updated = queryset.update(is_active=True)
+        self.message_user(request, f"{updated} comment(s) marked active.")
+    make_active.short_description = "Mark selected comments Active"
+
+    def make_inactive(self, request, queryset):
+        """Admin action to mark selected comments as inactive."""
+        updated = queryset.update(is_active=False)
+        self.message_user(request, f"{updated} comment(s) marked inactive.")
+    make_inactive.short_description = "Mark selected comments Inactive"
+    
