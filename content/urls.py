0832ffc5232d54976bda187cf7303bfbfd3e6125
Hatef Barin <amir.barin76@gmail.com>
2025-06-05 20:21:57 +0430

    Set up URLs for VideoViewSet

--- content/urls.py
+++ content/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ArticleViewSet
+from .views import ArticleViewSet, VideoViewSet
 
 
 
 router = DefaultRouter()
 router.register(r"articles", ArticleViewSet, basename="article")
+router.register(r"videos", VideoViewSet, basename="video")
 
 urlpatterns = [
     path('', include(router.urls)),
