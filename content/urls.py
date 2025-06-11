cb8555ad5d20d4f65397b474f8a26d1c79e23ec2
Hatef Barin <amir.barin76@gmail.com>
2025-06-11 20:57:32 +0430

    Set up URLs for FitnessMeasurementViewSet

--- content/urls.py
+++ content/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ArticleViewSet, VideoViewSet, ExerciseGuideViewSet
+from .views import ArticleViewSet, VideoViewSet, ExerciseGuideViewSet, FitnessMeasurementViewSet
 
 
 
@@ -10,6 +10,9 @@ router = DefaultRouter()
 router.register(r"articles", ArticleViewSet, basename="article")
 router.register(r"videos", VideoViewSet, basename="video")
 router.register(r"exercise-guides", ExerciseGuideViewSet, basename="exerciseguide")
+router.register(r"measurements", FitnessMeasurementViewSet, basename="fitnessmeasurement")
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
