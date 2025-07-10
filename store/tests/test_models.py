ba9dc95cd7642fee56ac18d0e943cdff71fb18cd
Hatef Barin <amir.barin76@gmail.com>
2025-07-10 14:48:19 +0430

    Add tests for CartModel

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -3,8 +3,10 @@ import datetime
 
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils import timezone
 
-from store.models import Product
+from store.models import Product, Cart, CartItem
+from core.models import CustomUser as User
 
 
 
@@ -74,3 +76,73 @@ class ProductModelTests(TestCase):
         )
         self.assertIsNotNone(product_with_image.image)
         self.assertIn("products/test_image.jpg", product_with_image.image.name)
+
+
+
+class CartModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(email="testuser@mail.com", username="testuser", password="pass1234")
+        self.cart = Cart.objects.create(user=self.user)
+
+        self.product1 = Product.objects.create(
+            name="Protein Bar",
+            description="A tasty protein bar",
+            price=Decimal("2.50"),
+            stock=100,
+        )
+        self.product2 = Product.objects.create(
+            name="Whey Protein",
+            description="5lb whey protein tub",
+            price=Decimal("49.99"),
+            stock=50,
+        )
+
+        self.cart_item1 = CartItem.objects.create(
+            cart=self.cart,
+            product=self.product1,
+            quantity=4,
+        )
+
+        self.cart_item2 = CartItem.objects.create(
+            cart=self.cart,
+            product=self.product2,
+            quantity=1,
+        )
+
+
+    def test_cart_str_representation(self):
+        self.assertEqual(str(self.cart), f"Cart of {self.user}")
+
+
+    def test_cart_created_at_auto_now_add(self):
+        self.assertIsNotNone(self.cart.created_at)
+        self.assertLessEqual(self.cart.created_at, timezone.now())
+
+
+    def test_cart_relationship_with_user(self):
+        self.assertEqual(self.cart.user, self.user)
+
+
+    def test_cart_items_relationship(self):
+        self.assertEqual(self.cart.products.count(), 2)
+        self.assertIn(self.product1, self.cart.products.all())
+        self.assertIn(self.product2, self.cart.products.all())
+
+
+    def test_total_price_calculation(self):
+        expected_total = (
+            self.product1.price * self.cart_item1.quantity +
+            self.product2.price * self.cart_item2.quantity
+        )
+        self.assertEqual(self.cart.total_price(), expected_total)
+
+
+    def test_cartitem_total_price_method(self):
+        self.assertEqual(self.cart_item1.total_price(), Decimal("10.00"))  # 4 x 2.50
+        self.assertEqual(self.cart_item2.total_price(), Decimal("49.99"))  # 1 x 49.99
+
+
+    def test_cart_empty_total_price(self):
+        empty_cart = Cart.objects.create(user=self.user)
+        self.assertEqual(empty_cart.total_price(), 0)
