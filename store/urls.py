727400de323694684a6d61d21f943c85a79e981f
Hatef Barin <amir.barin76@gmail.com>
2025-07-12 22:20:09 +0430

    Set up URLs for CartItemViewSet

---
+++ store/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedSimpleRouter
+
+from .views import ProductViewSet, CartItemViewSet
+
+
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+
+
+
+# /carts/{cart_pk}/items/
+cart_router = NestedSimpleRouter(router, r'carts', lookup='cart')
+cart_router.register(r'items', CartItemViewSet, basename='cart-items')
+
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(cart_router.urls)),
+]
