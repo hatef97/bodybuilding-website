cb9cdd3dc0205bcb24bf0f86c9e007096162a7c3
Hatef Barin <amir.barin76@gmail.com>
2025-07-16 17:45:54 +0430

    Add AddCartItemSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -83,6 +83,29 @@ class CartProductSeializer(serializers.ModelSerializer):
 
 
 
+class AddCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity']
+
+    def create(self, validated_data):
+        cart_id = self.context['cart_pk']
+
+        product = validated_data.get('product')
+        quantity = validated_data.get('quantity')
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product.id)
+            cart_item.quantity += quantity
+            cart_item.save()
+        except CartItem.DoesNotExist:
+            cart_item = CartItem.objects.create(cart_id=cart_id, **validated_data)
+
+        self.instance = cart_item
+        return cart_item
+
+
+
 class CartSerializer(serializers.ModelSerializer):
     """
     Serializer for Cart model, including nested items and total calculation.
